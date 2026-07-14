31178bb2b85f44870f51dfb9880c87ddc409ea22
leeyinsheng <lee.yin.sheng@gmail.com>
2026-07-14 13:31:35 +0800

    v15: add xv_pipeline to run_downloader.sh

--- src/run_downloader.sh
+++ src/run_downloader.sh
@@ -5,3 +5,4 @@ PROJECT_DIR="${PROJECT_DIR:-$HOME/Documents/Project/Adult Dream}"
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 cd "$PROJECT_DIR"
 /usr/bin/python3 "$PROJECT_DIR/src/download_tg_channel.py" >> "$PROJECT_DIR/download/download.log" 2>&1
+/usr/bin/python3 "$PROJECT_DIR/src/xv_pipeline.py" 5 >> "$PROJECT_DIR/download/xv_pipeline.log" 2>&1
